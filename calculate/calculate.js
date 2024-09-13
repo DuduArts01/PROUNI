47e6253b8a864caa3075be1a06120646df617ac1
Eduardo <eduardodemedeirossiqueira@gmail.com>
2024-09-13 20:30:54 -0300

    Version 1.5 beta
    Calculate function completed

--- calculate/calculate.js
+++ calculate/calculate.js
@@ -2,6 +2,9 @@ let res = document.getElementById("res");
 let res2 = document.querySelector("div#res2");
 let res3 = document.querySelector("div#res3");
 let res4 = document.querySelector("div#res4");
+let res5 = document.querySelector("div#res5");
+let res6 = document.querySelector("div#res6");
+
 
 function Calculator(){
     let num = document.querySelector("#income_calculate");
@@ -10,20 +13,23 @@ function Calculator(){
     let residents = resi.value;
 
     if(number / residents <= 2118){
-        Result_calculator_positive.style.display = "block";
+        Result_calculator_full_value.style.display = "block";
         res.innerHTML = `${number / residents}`;
-        res3.innerHTML = `${2118 - number / residents}`;
-        window.alert("uhuuu");
+        res2.innerHTML = `${2118 - number / residents}`;
+    } else if(number / residents <= 4236){
+        Result_calculator_partial_value.style.display = "block";
+        res3.innerHTML = `${number / residents}`;
+        res4.innerHTML = `${4236 - number / residents}`;
     } else{
         Result_calculator_negative.style.display = "block";
-        res2.innerHTML = `${number / residents}`;
-        res4.innerHTML = `${number / residents - 2118}`;
-        window.alert("breee");
+        res5.innerHTML = `${number / residents}`;
+        res6.innerHTML = `${number / residents - 2118}`;
     }
 
 }
 
 function out(){
-    Result_calculator_positive.style.display = "none";
+    Result_calculator_full_value.style.display = "none";
+    Result_calculator_partial_value.style.display = "none";
     Result_calculator_negative.style.display = "none";
 }
