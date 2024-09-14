2328c1554db030d2c29331ec1f36a95f0a29f141
Eduardo <eduardodemedeirossiqueira@gmail.com>
2024-09-13 22:18:49 -0300

    Version 1.5 alpha

--- calculate/calculate.js
+++ calculate/calculate.js
@@ -1,9 +1,9 @@
 let res = document.getElementById("res");
-let res2 = document.querySelector("div#res2");
-let res3 = document.querySelector("div#res3");
-let res4 = document.querySelector("div#res4");
-let res5 = document.querySelector("div#res5");
-let res6 = document.querySelector("div#res6");
+let res2 = document.querySelector("#res2");
+let res3 = document.querySelector("#res3");
+let res4 = document.querySelector("#res4");
+let res5 = document.querySelector("#res5");
+let res6 = document.querySelector("#res6");
 
 
 function Calculator(){
@@ -12,18 +12,24 @@ function Calculator(){
     let number = num.value;
     let residents = resi.value;
 
-    if(number / residents <= 2118){
+    if(!number.trim() && !residents.trim()){
+        window.alert("Por favor, Digite o valor da Renda e Moradores!");
+    } else if(!number.trim()){
+        window.alert("Por favor, Digite o valor da Renda!");
+    }else if(!residents.trim()){
+        window.alert("Por favor, Digite a quantidade de Moradores!");
+    } else if(number / residents <= 2118){
         Result_calculator_full_value.style.display = "block";
-        res.innerHTML = `${number / residents}`;
-        res2.innerHTML = `${2118 - number / residents}`;
+        res.innerHTML = `${(number / residents).toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})}`;
+        res2.innerHTML = `${(2118 - number / residents).toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})}`;
     } else if(number / residents <= 4236){
         Result_calculator_partial_value.style.display = "block";
-        res3.innerHTML = `${number / residents}`;
-        res4.innerHTML = `${4236 - number / residents}`;
+        res3.innerHTML = `${(number / residents).toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})}`;
+        res4.innerHTML = `${(4236 - number / residents).toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})}`;
     } else{
         Result_calculator_negative.style.display = "block";
-        res5.innerHTML = `${number / residents}`;
-        res6.innerHTML = `${number / residents - 2118}`;
+        res5.innerHTML = `${(number / residents).toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})}`;
+        res6.innerHTML = `${(number / residents - 2118).toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})}`;
     }
 
 }
